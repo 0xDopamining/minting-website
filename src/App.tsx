e58f3d168a1ba02f63001d4ef4587f13d9b4815c
@sascha.nievelstein <sascha.nievelstein@t-online.de>
2022-11-05 20:05:19 +0100

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {Button} from 'react-bootstrap'
+
+function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is the start</h1>
+        <Button>Lets go</Button>
+      </header>
+    </div>
+  )
+}
+
+export default App
