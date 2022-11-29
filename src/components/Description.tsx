adae661e46f687f0d461672f405fc0cda378e098
@sascha.nievelstein <sascha.nievelstein@t-online.de>
2022-11-29 15:31:44 +0100

    Fixed typos

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -6,9 +6,9 @@ const Description = () => {
     <Row className="my-4">
       <Col>
         <small>
-          <b>Disclaimer:</b> Dragons created during this event will only live on the test-net.
-          Meaning, you won't have them after the beta is closed. They give you, however, a better change of
-          receiving NFTs and other rewards when we launch on main-net.
+          <b>Disclaimer:</b> Dragons created during this event will only live on the testnet.
+          Meaning, you won't have them after the beta is closed. They give you, however, a better chance of
+          receiving NFTs and other rewards when we launch on mainnet.
         </small>
       </Col>
     </Row>
